f5c565bbf9dcfb40261dfbdb9da0d9a32435955e
LanternUnivers <wata1212naoki@gmail.com>
2025-02-16 11:54:44 +0900

    [success]station4の成功

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 // DO NOT DELETE
 
 import './App.css'
+import { useState } from 'react';
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg")
   return (
     <div>
       <header>
         <h1>Dogアプリ</h1>
       </header>
       <p>犬の画像を表示するサイトです</p>
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
+      <img src={dogUrl}></img>
     </div>
     
   )
