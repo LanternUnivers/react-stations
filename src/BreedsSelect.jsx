66af41e95c764f84dbccbab7777a654b9b22bc3d
LanternUnivers <wata1212naoki@gmail.com>
2025-02-17 12:05:39 +0900

    [success]station10の成功

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,18 @@
 // @ts-check
 
-export const BreedsSelect = () => {
-  return <></>
-}
+import React from 'react';
+
+export const BreedsSelect = ({ breeds, selectedBreed, onSelectBreed }) => {
+  return (
+    <select value={selectedBreed} onChange={(e) => onSelectBreed(e.target.value)}>
+      <option value="">犬種を選択してください</option>
+      {breeds.map((breed) => (
+        <option key={breed} value={breed}>
+          {breed}
+        </option>
+      ))}
+    </select>
+  );
+};
 
 export default BreedsSelect
--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,15 +1,19 @@
 // @ts-check
 
 import { useEffect, useState } from "react"
+import BreedsSelect from "./BreedsSelect";
 
 export const DogListContainer = () => {
-  const [breeds, setBreeds] = useState([])
+  const [breeds, setBreeds] = useState([]);
+  const [selectedBreed, setSelectedBreed] = useState("");
+
   useEffect(() => {
     const fetchBreeds = async () => {
       try {
         const response = await fetch('https://dog.ceo/api/breeds/list/all');
         const data = await response.json();
         const breedList = Object.keys(data.message); // 2階層目以降は無視
+        // @ts-ignore
         setBreeds(breedList);
       } catch (error) {
         console.error('犬種一覧の取得に失敗しました:', error);
@@ -18,7 +22,14 @@ export const DogListContainer = () => {
 
     fetchBreeds();
   }, []);
-  return <></>
+
+  return(
+    <div>
+      <h1>BreedsList</h1>
+      <BreedsSelect breeds={breeds} selectedBreed={selectedBreed} onSelectBreed={setSelectedBreed} />
+      <p>選択された犬種: {selectedBreed}</p>
+    </div>
+  );
 }
 
 export default DogListContainer
